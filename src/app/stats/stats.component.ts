b8b7b0418bc22e11e8bdcf35e38f0dafac9ab84d
mrinankchandna <Mrinankchandna17@gmail.com>
2025-07-13 00:30:38 +0530

    deleted extra file

--- src/app/stats/stats.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-stats',
-  standalone: true,
-  templateUrl: './stats.component.html',
-  styleUrls: ['./stats.component.css']
-})
-export class StatsComponent {}
--- src/main.server.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app';
-import { config } from './app/app.config.server';
-
-// This function is used to bootstrap the application on the server
-export default function bootstrap() {
-  return bootstrapApplication(AppComponent, config);
-}
